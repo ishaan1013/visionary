101039a3342f151f7024e9166004de47baf13777
Adrian Tang <adrian.s.tang@gmail.com>
2023-09-16 16:58:09 -0400

    created generate_notes function

--- cohere.py
+++ cohere.py
@@ -2,25 +2,23 @@ import json
 import requests
 import config
 
-# comment this section out if you dont want the prompt to read from another file and remove +data from prompt variable
-# choose data file
-filepath = 'jsondata\\algebra.txt'
-jsondata = open(filepath,'r')
-data = ""
-for line in jsondata:
-    data = data + line
-jsondata.close()
+def generate_notes(data, topic="", description=""):
+    #data is expected to be a string
+    
+    if topic!="":
+        topic = f"Topic: {topic}\n"
+    if description!="":
+        description = f"Description: {topic}\n"
+    #write prompt here
+    prompt = F"Generate notes in point-form using the following data as hints about points from the lecture. Consider the following parameters: \"size\" refers to the size and significance of the text, \"confidence\" refers to how likely the extracted text might match the original, and use autocorrect.possibility as the extracted text. {topic} {description} Data: {data}\n"
 
-# write prompt here
-lecturetopic = "abstract algebra and ring theory"
-prompt = "Generate notes about "+lecturetopic+" in point-form using the following data as hints about points from the lecture. extracted from a calculus lecture presentation. Consider the following parameters: \"size\" refers to the size and significance of the text, \"confidence\" refers to how likely the extracted text might match the original, and use autocorrect.possibility as the extracted text. Data: \n"+data
-
-headers = {
+    headers = {
     'Authorization': 'BEARER '+config.api_key,
     'Content-Type': 'application/json',
-}
+    }
 
-json_data = {
+    # model/output parameters
+    json_data = {
     'model': 'command-nightly',
     "prompt": prompt,
     'max_tokens': 400,
@@ -28,10 +26,23 @@ json_data = {
     'k': 0,
     'stop_sequences': [],
     'return_likelihoods': 'NONE',
-}
+    }
 
-response = requests.post('https://api.cohere.ai/v1/generate', headers=headers, json=json_data)
+    response = requests.post('https://api.cohere.ai/v1/generate', headers=headers, json=json_data)
+    response_dict = json.loads(response.text)
 
-response_dict = json.loads(response.text)
+    return response_dict["generations"][0]["text"]
 
-print(response_dict["generations"][0]["text"])
+
+# comment this section out if you dont want the prompt to read from another file and remove +data from prompt variable
+# choose data file
+
+filepath = 'jsondata\\algebra.txt'
+jsondata = open(filepath,'r')
+datastring = ""
+for line in jsondata:
+    datastring = datastring + line
+jsondata.close()
+
+x = generate_notes(datastring, "abstract algebra and ring theory")
+print(x)
