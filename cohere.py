1dec44b2b263279503e29b976a777fbaabb228ea
tan-ad <adrian.s.tang@gmail.com>
2023-09-16 14:27:25 -0400

    added jsondata and modified cohere.py

--- cohere.py
+++ cohere.py
@@ -1,28 +1,29 @@
 import json
 import requests
-import os
 
-# comment this section out if you dont want the prompt to read from another file
-jsondata = open('jsondata.txt','r')
+# comment this section out if you dont want the prompt to read from another file and remove +data from prompt variable
+# choose data file
+filepath = 'jsondata\\algebra.txt'
+jsondata = open(filepath,'r')
 data = ""
 for line in jsondata:
     data = data + line
-#print("data:", data, "\n")
 jsondata.close()
 
 # write prompt here
-prompt = "Generate notes about MUSIC HISTORY in point-form using the following data as hints about points from the lecture. extracted from a calculus lecture presentation. Consider the following parameters: \"size\" refers to the size and significance of the text, \"confidence\" refers to how likely the extracted text might match the original, and use autocorrect.possibility as the extracted text. Data: \n"+data
+lecturetopic = "abstract algebra and ring theory"
+prompt = "Generate notes about "+lecturetopic+" in point-form using the following data as hints about points from the lecture. extracted from a calculus lecture presentation. Consider the following parameters: \"size\" refers to the size and significance of the text, \"confidence\" refers to how likely the extracted text might match the original, and use autocorrect.possibility as the extracted text. Data: \n"+data
 
 headers = {
-    'Authorization': 'BEARER RmQ6r0yPvLmaghPGooXaIF4W8RrhlH8tJECl6dGz',
+    'Authorization': 'BEARER Z8VlOl9MdeMTiCNGblrNcoikN3XkIzuSJVMwm8wg',
     'Content-Type': 'application/json',
 }
 
 json_data = {
-    'model': 'command',
-          "prompt": prompt,
-    'max_tokens': 300,
-    'temperature': 0.9,
+    'model': 'command-nightly',
+    "prompt": prompt,
+    'max_tokens': 400,
+    'temperature': 0.5,
     'k': 0,
     'stop_sequences': [],
     'return_likelihoods': 'NONE',
@@ -33,8 +34,3 @@ response = requests.post('https://api.cohere.ai/v1/generate', headers=headers, j
 response_dict = json.loads(response.text)
 
 print(response_dict["generations"][0]["text"])
-
-# Note: json_data will not be serialized by requests
-# exactly as it was in the original request.
-#data = '{\n      "model": "command",\n      "prompt": "tell me a joke",\n      "max_tokens": 300,\n      "temperature": 0.9,\n      "k": 0,\n      "stop_sequences": [],\n      "return_likelihoods": "NONE"\n    }'
-#response = requests.post('https://api.cohere.ai/v1/generate', headers=headers, data=data)
