43cabe61c1765280e4f89644c5097c6d25e6ed96
Ryan Zhu <rz.ryanzhu@gmail.com>
2023-09-16 12:45:25 -0400

    fix send image

--- lib/sendImage.ts
+++ lib/sendImage.ts
@@ -1,17 +1,20 @@
-export const sendImage = async (theme: string, blob: any) => {
+export const sendImage = async (theme: string, blob: Blob) => {
   try {
-    blob = await blob.arrayBuffer();
-    const view = new Uint8Array(blob);
-    const res = await fetch("http://127.0.0.1:5000/api/text_from_image", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+    const buffer = await blob.arrayBuffer();
+    const view = new Uint8Array(buffer);
+    const res = await fetch(
+      "http://https://visionary-backend-px6z5q8n9-ryan-zhu-music.vercel.app/api/text_from_image",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          blob: view,
+          theme: theme,
+        }),
       },
-      body: JSON.stringify({
-        blob: view,
-        theme: theme,
-      }),
-    });
+    );
     const data = await res.json();
     console.log(data);
   } catch (err) {
