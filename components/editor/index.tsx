026e885d9864903d30d0cdc317fe36ddf8141e01
Ishaan Dey <ishaan1013@gmail.com>
2023-09-16 11:50:40 -0400

    get localstorage on debounce

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -84,7 +84,9 @@ export default function NoteEditor({
             class: `prose-base prose prose-zinc prose prose-h1:font-semibold prose-h1:text-3xl prose-h1:mb-0.5 prose-h2:font-semibold  prose-h2:text-2xl prose-h3:font-semibold  prose-h3:text-xl prose-h4:font-medium  prose-h4:text-lg font-satoshi focus:outline-none max-w-full`,
           },
         }}
-        onDebouncedUpdate={(data) => console.log(data)}
+        onDebouncedUpdate={() =>
+          console.log(localStorage.getItem("novel__content"))
+        }
         className="prose w-full max-w-full border-none bg-transparent p-0 px-8 shadow-none lg:px-4 xl:px-0"
       />
     </>
