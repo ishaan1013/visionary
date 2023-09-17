a92c82292670c2e252a7f278ddb4dcc139c53665
Adrian Tang <adrian.s.tang@gmail.com>
2023-09-17 03:55:03 -0400

    added openai.py to convert note to json format

--- cohere.py
+++ cohere.py
@@ -11,16 +11,17 @@ def generate_notes(data, topic="", description=""):
         description = f"Description: {topic}\n"
     
     #write prompt here
-    prompt = f"Generate notes in point-form using the following data as hints about points from the lecture. Include headers in markdown format. Consider the following parameters: \"size\" refers to the size and significance of the text so phrases with larger size are more important, use autocorrect.possibility as the extracted text, and autocorrect.confidence refers to the probability that text is accurate. \n {topic} {description} Data: {data}\n Output in the format"
+    prompt = f"Please analyze the provided data and generate concise notes summarizing the key insights, trends, and important details. Ensure that the notes are organized logically and follow a clear structure. Use bullet points or numbered lists where appropriate for easy readability. Please prioritize accuracy and clarity in the notes. If there are any critical recommendations or action points based on the analysis, include those as well. Include headers for the general topic and subtopics in markdown format.Consider the following parameters: \"size\" refers to the size and significance of the text so phrases with larger size are more important, use autocorrect.possibility as the extracted text, and autocorrect.confidence refers to the probability that text is accurate. \n {topic} {description} Data: {data}\n"
 
     headers = {
-    'Authorization': 'BEARER '+config.api_key,
+    'Authorization': 'BEARER '+config.cohere_key,
     'Content-Type': 'application/json',
     }
 
     # model/output parameters
     json_data = {
     'model': 'command-nightly',
+    "preamble_override": "",
     "prompt": prompt,
     'max_tokens': 2048,
     'temperature': 0.1,
@@ -37,12 +38,17 @@ def generate_notes(data, topic="", description=""):
 # comment this section out if you dont want the prompt to read from another file and remove +data from prompt variable
 # choose data file
 
-filepath = 'jsondata\\algebra.txt'
-jsondata = open(filepath,'r')
+file_1_path = 'jsondata\\algebra.txt'
+file_2_path = 'jsondata\\new_note.txt'
+json_data = open(file_1_path,'r')
+new_note = open(file_2_path, 'w')
 datastring = ""
-for line in jsondata:
+for line in json_data:
     datastring = datastring + line
-jsondata.close()
 
-x = generate_notes(datastring, "abstract algebra and ring theory")
-print(x)
+notes = generate_notes(datastring, "abstract algebra and ring theory")
+print(notes)
+
+new_note.write(notes)
+json_data.close()
+new_note.close()
